f55643bda30af2072bad27488ae3b9a98c2cd213
simshadows <contact@simshadows.com>
2020-02-24 15:39:26 +1100

    Implemented Attack Boost calculations.

--- run.py
+++ run.py
@@ -24,12 +24,16 @@ from database_misc    import *
 SHARPNESS_LEVEL_NAMES   = ("Red", "Orange", "Yellow", "Green", "Blue", "White", "Purple")
 RAW_SHARPNESS_MODIFIERS = (0.5,   0.75,     1.0,      1.05,    1.2,    1.32,    1.39    )
 
-RAW_BLUNDER_MULTIPLIER = 0.75 # If you have negative affinity, this is the multiplier instead.
-CRITICAL_BOOST_RAW_CRIT_MULTIPLIERS = (1.25, 1.30, 1.35, 1.40)
-#                             level =  0     1     2     3
+ATTACK_BOOST_ADDED_ATTACK_POWER        = (0, 3, 6, 9, 12, 15, 18, 21)
+ATTACK_BOOST_ADDED_AFFINITY_PERCENTAGE = (0, 0, 0, 0, 5,  5,  5,  5 )
+#                                level =  0  1  2  3  4   5   6   7
 
 CRITICAL_EYE_ADDED_AFFINITY_PERCENTAGE = (0, 5, 10, 15, 20, 25, 30, 40)
 #                                level =  0  1  2   3   4   5   6   7
+
+RAW_BLUNDER_MULTIPLIER = 0.75 # If you have negative affinity, this is the multiplier instead.
+CRITICAL_BOOST_RAW_CRIT_MULTIPLIERS = (1.25, 1.30, 1.35, 1.40)
+#                             level =  0     1     2     3
 
 def print_debugging_statistics():
     print("=== Application Statistics ===")
@@ -40,29 +44,54 @@ def print_debugging_statistics():
     return
 
 
-def calculate_highest_sharpness_modifier(weapon_maximum_sharpness, handicraft_level):
+# Returns both the values of the new sharpness bar, and the highest sharpness level.
+# The new sharpness bar corresponds to the indices in RAW_SHARPNESS_LEVEL_MODIFIERS and SHARPNESS_LEVEL_NAMES.
+# The highest sharpness level also corresponds to the same indices.
+def actual_sharpness_level_values(weapon_maximum_sharpness, handicraft_level):
     assert (handicraft_level >= 0) and (handicraft_level <= 5)
     assert (len(weapon_maximum_sharpness) == 7) and (len(RAW_SHARPNESS_MODIFIERS) == 7)
 
-    # We traverse the weapon sharpness bar in reverse, then
-    # keep subtracting missing handicraft levels until we stop.
+    # We traverse the weapon sharpness bar in reverse, subtracting based on missing handicraft levels.
     points_to_subtract = (5 - handicraft_level) * 10
+    stop_level = 7
+    actual_values = []
     for (level, points) in reversed(list(enumerate(weapon_maximum_sharpness))):
-        points_to_subtract -= weapon_maximum_sharpness[level]
-        if points_to_subtract < 0:
-            break
+        if points > points_to_subtract:
+            points_to_subtract = 0
+            actual_values.insert(0, points - points_to_subtract)
+        else:
+            stop_level = level
+            points_to_subtract -= points
+            actual_values.insert(0, 0)
 
-    #print(f"Points of sharpness until next level = {-points_to_subtract}")
-    #print()
-    
-    maximum_sharpness_level = level
-    return RAW_SHARPNESS_MODIFIERS[maximum_sharpness_level]
+    assert len(actual_values) == 7
+    return (tuple(actual_values), stop_level - 1)
+
+# This will be useful in the future for algorithm performance optimization.
+#def calculate_highest_sharpness_modifier(weapon_maximum_sharpness, handicraft_level):
+#    assert (handicraft_level >= 0) and (handicraft_level <= 5)
+#    assert (len(weapon_maximum_sharpness) == 7) and (len(RAW_SHARPNESS_MODIFIERS) == 7)
+#
+#    # We traverse the weapon sharpness bar in reverse, then
+#    # keep subtracting missing handicraft levels until we stop.
+#    points_to_subtract = (5 - handicraft_level) * 10
+#    for (level, points) in reversed(list(enumerate(weapon_maximum_sharpness))):
+#        points_to_subtract -= weapon_maximum_sharpness[level]
+#        if points_to_subtract < 0:
+#            break
+#
+#    #print(f"Points of sharpness until next level = {-points_to_subtract}")
+#    #print()
+#    
+#    maximum_sharpness_level = level
+#    return RAW_SHARPNESS_MODIFIERS[maximum_sharpness_level]
 
 
 SkillsContribution = namedtuple(
     "SkillsContribution",
     [
-        "highest_sharpness_modifier",
+        "handicraft_level",
+        "added_attack_power",
         "raw_critical_multiplier",
         "added_raw_affinity_percentage",
     ],
@@ -70,13 +99,19 @@ SkillsContribution = namedtuple(
 def calculate_skills_contribution(skills_dict, maximum_sharpness_values):
     skills_dict = clipped_skills_defaultdict(skills_dict)
 
+    attack_boost_ap = ATTACK_BOOST_ADDED_ATTACK_POWER[skills_dict[Skill.ATTACK_BOOST]]
+    attack_boost_aff = ATTACK_BOOST_ADDED_AFFINITY_PERCENTAGE[skills_dict[Skill.ATTACK_BOOST]]
+
+    critical_eye_aff = CRITICAL_EYE_ADDED_AFFINITY_PERCENTAGE[skills_dict[Skill.CRITICAL_EYE]]
+
+    added_attack_power = attack_boost_ap
+    added_raw_affinity = critical_eye_aff + attack_boost_aff
+
     ret = SkillsContribution(
-            highest_sharpness_modifier = \
-                calculate_highest_sharpness_modifier(maximum_sharpness_values, skills_dict[Skill.HANDICRAFT]),
-            raw_critical_multiplier = \
-                CRITICAL_BOOST_RAW_CRIT_MULTIPLIERS[skills_dict[Skill.CRITICAL_BOOST]],
-            added_raw_affinity_percentage = \
-                CRITICAL_EYE_ADDED_AFFINITY_PERCENTAGE[skills_dict[Skill.CRITICAL_EYE]],
+            handicraft_level              = skills_dict[Skill.HANDICRAFT],
+            added_attack_power            = added_attack_power,
+            raw_critical_multiplier       = CRITICAL_BOOST_RAW_CRIT_MULTIPLIERS[skills_dict[Skill.CRITICAL_BOOST]],
+            added_raw_affinity_percentage = added_raw_affinity
         )
     return ret
 
@@ -130,6 +165,7 @@ PerformanceValues = namedtuple(
     "PerformanceValues",
     [
         "efr",
+        "sharpness_values",
     ],
 )
 def lookup(weapon_name, skills_dict):
@@ -141,17 +177,23 @@ def lookup(weapon_name, skills_dict):
     maximum_sharpness_values = weapon.maximum_sharpness
     skills_contribution = calculate_skills_contribution(skills_dict, maximum_sharpness_values)
 
+    handicraft_level = skills_contribution.handicraft_level
+    sharpness_values, highest_sharpness_level = actual_sharpness_level_values(maximum_sharpness_values, handicraft_level)
+
+    item_attack_power = POWERCHARM_ATTACK_POWER + POWERTALON_ATTACK_POWER
+
     kwargs = {}
     kwargs["weapon_attack_power"]        = weapon.attack
     kwargs["weapon_type"]                = weapon.type
     kwargs["weapon_affinity_percentage"] = weapon.affinity
-    kwargs["added_attack_power"]         = POWERCHARM_ATTACK_POWER + POWERTALON_ATTACK_POWER
+    kwargs["added_attack_power"]         = skills_contribution.added_attack_power + item_attack_power
     kwargs["added_affinity_percentage"]  = skills_contribution.added_raw_affinity_percentage
-    kwargs["raw_sharpness_modifier"]     = skills_contribution.highest_sharpness_modifier
+    kwargs["raw_sharpness_modifier"]     = RAW_SHARPNESS_MODIFIERS[highest_sharpness_level]
     kwargs["raw_crit_multiplier"]        = skills_contribution.raw_critical_multiplier
 
     ret = PerformanceValues(
-            efr = calculate_efr(**kwargs),
+            efr                = calculate_efr(**kwargs),
+            sharpness_values   = sharpness_values,
         )
     return ret
 
@@ -163,14 +205,26 @@ def search_command():
 def lookup_command(weapon_name):
     skills_dict = {
             Skill.HANDICRAFT: 5,
-            Skill.CRITICAL_EYE: 1,
+            Skill.CRITICAL_EYE: 7,
             Skill.CRITICAL_BOOST: 3,
+            Skill.ATTACK_BOOST: 7,
         }
 
-    print("\n".join(f"{skill}: {level}" for (skill, level) in clipped_skills_defaultdict(skills_dict).items()))
+    print("Skills:")
+    print("\n".join(f"   {skill.value.name} {level}" for (skill, level) in clipped_skills_defaultdict(skills_dict).items()))
     print()
 
     p = lookup(weapon_name, skills_dict)
+
+    print("Sharpness values:")
+    print(f"   Red:    {p.sharpness_values[0]} hits")
+    print(f"   Orange: {p.sharpness_values[1]} hits")
+    print(f"   Yellow: {p.sharpness_values[2]} hits")
+    print(f"   Green:  {p.sharpness_values[3]} hits")
+    print(f"   Blue:   {p.sharpness_values[4]} hits")
+    print(f"   White:  {p.sharpness_values[5]} hits")
+    print(f"   Purple: {p.sharpness_values[6]} hits")
+    print()
 
     print("EFR = " + str(p.efr))
     return
@@ -185,6 +239,7 @@ def tests_passed():
 
     # This function will leave skills_dict with the skill at max_level.
     def test_with_incrementing_skill(skill, max_level, expected_efrs):
+        assert max_level == skill.value.limit
         assert len(expected_efrs) == (max_level + 1)
         for level in range(max_level + 1):
             skills_dict[skill] = level
@@ -204,6 +259,8 @@ def tests_passed():
     test_with_incrementing_skill(Skill.CRITICAL_BOOST, 3, [411.01, 413.98, 416.95, 419.92])
     print("Incrementing Critical Eye.")
     test_with_incrementing_skill(Skill.CRITICAL_EYE, 7, [419.92, 427.84, 435.77, 443.69, 451.61, 459.53, 467.46, 483.30])
+    print("Incrementing Attack Boost.")
+    test_with_incrementing_skill(Skill.ATTACK_BOOST, 7, [483.30, 488.39, 493.48, 498.57, 511.91, 517.08, 522.25, 527.42])
 
     #weapon = "Jagras Deathclaw II"
 
